7d98220e4030820490e13943024c067a2fe59f7c
teraomoyu <dmntc.5299@gmail.com>
2022-08-22 23:32:46 +0900

    add: twwディレクトリ

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'j30n=mk!n!g!n=z@*j#mjex!p&lfl)6crbsyl@!n1fuamvr_4o'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'composeexample.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgrespassword',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
