d2e0ca3d1a2f5839bfaa9ae02e848caddd468f45
teraomoyu <dmntc.5299@gmail.com>
2022-08-24 00:01:30 +0900

    add: createページ

--- tww/models.py
+++ tww/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Mood(models.Model):
+    mood_text = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.mood_text
+
+class Writing(models.Model):
+    writing_text = models.TextField(max_length=400)
+    user_name = models.CharField(max_length=20)
+    word1 = models.CharField(max_length=100)
+    word2 = models.CharField(max_length=100)
+    word3 = models.CharField(max_length=100)
+    mood = models.ForeignKey(Mood, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.writing_text
--- tww/views.py
+++ tww/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Mood
+import random
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def create(request):
+    moods = Mood.objects.all()
+    random_index = random.randint(0, len(moods)-1)
+    random_mood = moods[random_index].mood_text
+    return HttpResponse(f"キーワードは「単語１」，「単語２」，「単語３」です．{random_mood}雰囲気で書きましょう．")
