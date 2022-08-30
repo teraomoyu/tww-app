9b329cfd8852ed445522bb344c7ae6e5f0586075
teraomoyu <dmntc.5299@gmail.com>
2022-08-30 20:04:12 +0900

    add:投稿機能

--- tww/urls.py
+++ tww/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = "tww"
 urlpatterns = [
-    path('create/', views.create, name='create'),
-    path('all/', views.view_all_writings, name='view_all_writings'),
-    path('<int:writing_id>/view/', views.view, name='view'),
+    path("create/", views.create, name="create"),
+    path("<int:random_index>/upload/", views.upload, name="upload"),
+    path("all/", views.view_all_writings, name="view_all_writings"),
+    path("<int:writing_id>/view/", views.view, name="view"),
 ]
--- tww/views.py
+++ tww/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 import random
 
 from .models import Mood, Writing
@@ -8,15 +9,32 @@ from .models import Mood, Writing
 
 def create(request):
     moods = Mood.objects.all()
-    random_index = random.randint(0, len(moods)-1)
+    random_index = random.randint(0, len(moods) - 1)
     random_mood = moods[random_index].mood_text
-    context = {"random_mood": random_mood}
-    return render(request, "tww/index.html", context)
+    context = {"random_index": random_index, "random_mood": random_mood}
+    return render(request, "tww/create.html", context)
+
+
+def upload(request, random_index):
+    moods = Mood.objects.all()
+    w = Writing(
+        writing_text=request.POST.get("writing-text"),
+        user_name=request.POST.get("user-name"),
+        word1="坊主",
+        word2="屏風",
+        word3="上手",
+        mood=moods[random_index],
+    )
+    w.save()
+    writing_id = w.id
+    return HttpResponseRedirect(reverse("tww:view", args=(writing_id,)))
+
 
 def view_all_writings(request):
     writings = Writing.objects.all()
     context = {"writings": writings}
     return render(request, "tww/view-all-writings.html", context)
+
 
 def view(request, writing_id):
     writing = get_object_or_404(Writing, pk=writing_id)
