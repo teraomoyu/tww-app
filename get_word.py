05f871537360d9c9d88e26f32d63b93fb26fabd9
teraomoyu <dmntc.5299@gmail.com>
2022-10-23 17:13:42 +0900

    add:形容詞一覧

--- get_word.py
+++ get_word.py
@@ -4,21 +4,12 @@ import csv
 import re
 from pdb import set_trace
 
-f = open("words.csv", "w")
-writer = csv.writer(f)
-load_urls = [
-    "https://jn1et.com/list-noun/",
-    # "https://jn1et.com/list-adjective/",
-    # "https://jn1et.com/list-verb/",
-    # "https://jn1et.com/list-adverbs/",
-    # "https://jn1et.com/list-conjunction-interrogative/"
-]
-rows = []
-for load_url in load_urls:
-    html = requests.get(load_urls[0])
+
+def get_noun(load_urls):
+    html = requests.get(load_urls["noun"])
     soup = BeautifulSoup(html.content, "html.parser")
     tables = soup.find_all("table")
-   
+
     tr_tags = [i.find_all("tr") for i in tables]
     words = [
         [
@@ -39,4 +30,41 @@ for load_url in load_urls:
         if index % 2 != 0
     ]
     words = sum(words, [])
-    print(len(words))
+    print(words)
+    return words
+
+
+def get_adjective(load_urls):
+    html = requests.get(load_urls["adjective"])
+    soup = BeautifulSoup(html.content, "html.parser")
+    tables = soup.find_all("table")
+
+    tr_tags = [i.find_all("tr")[1] for i in tables]
+    words = [
+        re.sub("\xa0|\n|\u2002|＊|（(.*)）|\((.*)\)", "", w)
+        for tr_tag in tr_tags
+        for td in tr_tag.find_all("td")
+        for w in re.split("\n| ", td.get_text())
+        if re.search(
+            r"[ぁ-んァ-ン一丁-齢龍]", re.sub("\xa0|\n|\u2002|＊|（(.*)）|\((.*)\)", "", w)
+        )
+    ]
+    words = list(set(words))
+    set_trace()
+    return words
+
+
+def main():
+    load_urls = {
+        "noun": "https://jn1et.com/list-noun/",  # 名詞
+        "adjective": "https://jn1et.com/list-adjective/",  # 形容詞
+        "verb": "https://jn1et.com/list-verb/",  # 動詞
+        "adverb": "https://jn1et.com/list-adverbs/",  # 副詞
+        "conjunction": "https://jn1et.com/list-conjunction-interrogative/",  # 接続詞・疑問詞
+    }
+    # nouns = get_noun(load_urls)
+    adjectives = get_adjective(load_urls)
+
+
+if __name__ == "__main__":
+    main()
